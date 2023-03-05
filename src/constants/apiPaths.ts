263653c30d6b80e1a3b13b2107e4d920cea04bff
Balabanov, Oleksandr <oleksandr.balabanov@mckesson.com>
2023-03-05 18:04:56 +0100

    [Task 4] - create Products-Service:    - changed data objects structure according to requirements (used Product type only which now include count field as well)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://nae6g042y6.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://nae6g042y6.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://nae6g042y6.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://nae6g042y6.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://nae6g042y6.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://qmbbj6kgxd.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://qmbbj6kgxd.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://qmbbj6kgxd.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://qmbbj6kgxd.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://qmbbj6kgxd.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
